8d460d53d1e575a5a553a40f5d37bce03f15414b
Prajjawalk <prajjawalkhandelwal@gmail.com>
2022-09-27 14:57:46 +0530

    modified chain_test.go

--- chain/chain_test.go
+++ chain/chain_test.go
@@ -5,10 +5,21 @@ import (
 	"fmt"
 	"math/big"
 	"os"
+	"path"
 	"testing"
 
+	log "github.com/sirupsen/logrus"
+	"github.com/theQRL/go-qrllib/dilithium"
+	"github.com/theQRL/go-qrllib/xmss"
+	"github.com/theQRL/zond/block"
+	"github.com/theQRL/zond/common"
+	"github.com/theQRL/zond/core/rawdb"
+	state2 "github.com/theQRL/zond/core/state"
 	"github.com/theQRL/zond/metadata"
+	"github.com/theQRL/zond/ntp"
+	"github.com/theQRL/zond/protos"
 	"github.com/theQRL/zond/state"
+	"github.com/theQRL/zond/transactions"
 	"go.etcd.io/bbolt"
 )
 
@@ -172,179 +183,160 @@ func TestGetStartingNonFinalizedEpoch(t *testing.T) {
 	}
 }
 
-// func TestGetSlotLeaderDilithiumPKBySlotNumber(t *testing.T) {
-// 	slotNumber := uint64(201)
-// 	parentSlotNumber := uint64(150)
-// 	parentSlotNumber2 := uint64(50)
-// 	parentHeaderHash := sha256.Sum256([]byte("parentHeaderHash"))
-// 	parentHeaderHash2 := sha256.Sum256([]byte("parentHeaderHash2"))
-// 	totalStakeAmount := []byte("100")
-// 	trieRoot := common.Hash{}
+func TestGetSlotLeaderDilithiumPKBySlotNumber(t *testing.T) {
+	slotNumber := uint64(201)
+	parentSlotNumber := uint64(150)
+	parentSlotNumber2 := uint64(50)
+	parentHeaderHash := sha256.Sum256([]byte("parentHeaderHash"))
+	parentHeaderHash2 := sha256.Sum256([]byte("parentHeaderHash2"))
+	totalStakeAmount := []byte("100")
+	trieRoot := common.Hash{}
 
-// 	parentBlockMetadata := metadata.NewBlockMetaData(parentHeaderHash2, parentHeaderHash, parentSlotNumber, totalStakeAmount, trieRoot)
-// 	parentBlockMetadata2 := metadata.NewBlockMetaData(parentHeaderHash2, parentHeaderHash2, parentSlotNumber2, totalStakeAmount, trieRoot)
+	parentBlockMetadata := metadata.NewBlockMetaData(parentHeaderHash2, parentHeaderHash, parentSlotNumber, totalStakeAmount, trieRoot)
+	parentBlockMetadata2 := metadata.NewBlockMetaData(parentHeaderHash2, parentHeaderHash2, parentSlotNumber2, totalStakeAmount, trieRoot)
 
-// 	validatorDilithium := dilithium.New()
-// 	validatorDilithiumPK := validatorDilithium.GetPK()
-// 	validatorDilithium2 := dilithium.New()
-// 	validatorDilithium2PK := validatorDilithium2.GetPK()
-// 	var validators [][]byte
-// 	validators = append(validators, validatorDilithiumPK[:])
-// 	validators = append(validators, validatorDilithium2PK[:])
-// 	epochMetadata := metadata.NewEpochMetaData(1, parentHeaderHash2, validators)
+	validatorDilithium := dilithium.New()
+	validatorDilithiumPK := validatorDilithium.GetPK()
+	validatorDilithium2 := dilithium.New()
+	validatorDilithium2PK := validatorDilithium2.GetPK()
+	var validators [][]byte
+	validators = append(validators, validatorDilithiumPK[:])
+	validators = append(validators, validatorDilithium2PK[:])
+	epochMetadata := metadata.NewEpochMetaData(1, parentHeaderHash2, validators)
 
-// 	finalizedBlockHeaderHash := sha256.Sum256([]byte("finalizedHeaderHash"))
-// 	finalizedBlockSlotNumber := uint64(202)
-// 	lastBlockHeaderHash := sha256.Sum256([]byte("lastBlockHeaderhash"))
-// 	lastBlockSlotNumber := uint64(155)
-// 	mainChainMetaData := metadata.NewMainChainMetaData(finalizedBlockHeaderHash, finalizedBlockSlotNumber,
-// 		lastBlockHeaderHash, lastBlockSlotNumber)
+	finalizedBlockHeaderHash := sha256.Sum256([]byte("finalizedHeaderHash"))
+	finalizedBlockSlotNumber := uint64(202)
+	lastBlockHeaderHash := sha256.Sum256([]byte("lastBlockHeaderhash"))
+	lastBlockSlotNumber := uint64(155)
+	mainChainMetaData := metadata.NewMainChainMetaData(finalizedBlockHeaderHash, finalizedBlockSlotNumber,
+		lastBlockHeaderHash, lastBlockSlotNumber)
 
-// 	state, _ := state.NewState("./", "testStateDb.txt")
-// 	defer os.Remove("testStateDb.txt")
-// 	chain := NewChain(state)
+	state, _ := state.NewState("./", "testStateDb.txt")
+	defer os.Remove("testStateDb.txt")
+	chain := NewChain(state)
 
-// 	// networkId := uint64(1)
-// 	// timestamp := ntp.GetNTP().Time()
+	err := state.DB().DB().Update(func(tx *bbolt.Tx) error {
+		bucketName := metadata.GetBlockBucketName(parentHeaderHash)
+		blockBucket := tx.Bucket([]byte(bucketName))
+		if blockBucket == nil {
+			_, err := tx.CreateBucket(bucketName)
+			if err != nil {
+				return fmt.Errorf("create bucket: %s", err)
+			}
+			return nil
+		}
 
-// 	// var txs []*protos.Transaction
-// 	// txn1 := &protos.Transaction{
-// 	// 	NetworkId:  1,
-// 	// 	MasterAddr: address_[:],
-// 	// 	Fee:        uint64(10),
-// 	// 	Nonce:      uint64(10),
-// 	// 	Pk:         validatorXmssPK[:],
-// 	// 	Signature:  []byte("blocksignature"),
-// 	// }
-// 	// txs = append(txs, txn1)
-// 	//var protocolTxs []*protos.ProtocolTransaction
-// 	// ptxn1 := &protos.ProtocolTransaction{
-// 	// 	NetworkId: 1,
-// 	// 	Nonce:     10,
-// 	// 	Pk:        validators[0],
-// 	// 	Signature: []byte("blocksignature"),
-// 	// }
-// 	// protocolTxs = append(protocolTxs, ptxn1)
-// 	//lastCoinBaseNonce := uint64(10)
+		return nil
+	})
 
-// 	//newBlock := block.NewBlock(networkId, timestamp, validatorDilithiumPK[:], parentSlotNumber, parentHeaderHash2, txs, protocolTxs, lastCoinBaseNonce)
+	if err != nil {
+		t.Errorf("unexpected error committing to database (%v)", err)
+	}
 
-// 	err := state.DB().DB().Update(func(tx *bbolt.Tx) error {
-// 		mainBucket := tx.Bucket([]byte("DB"))
-// 		if mainBucket == nil {
-// 			_, err := tx.CreateBucket([]byte("DB"))
-// 			if err != nil {
-// 				return fmt.Errorf("create bucket: %s", err)
-// 			}
-// 			return nil
-// 		}
+	err = state.DB().DB().Update(func(tx *bbolt.Tx) error {
+		bucketName := metadata.GetBlockBucketName(parentHeaderHash2)
+		blockBucket := tx.Bucket([]byte(bucketName))
+		if blockBucket == nil {
+			_, err := tx.CreateBucket(bucketName)
+			if err != nil {
+				return fmt.Errorf("create bucket: %s", err)
+			}
+			return nil
+		}
 
-// 		err := mainChainMetaData.Commit(mainBucket)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		err = parentBlockMetadata.Commit(mainBucket)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		err = parentBlockMetadata2.Commit(mainBucket)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		err = epochMetadata.Commit(mainBucket)
-// 		if err != nil {
-// 			return err
-// 		}
+		return nil
+	})
 
-// 		return nil
-// 	})
+	if err != nil {
+		t.Errorf("unexpected error committing to database (%v)", err)
+	}
 
-// 	if err != nil {
-// 		t.Errorf("unexpected error committing to database (%v)", err)
-// 	}
+	db2, err := rawdb.NewLevelDBDatabaseWithFreezer(
+		path.Join(chain.config.User.DataDir(), chain.config.Dev.DB2Name), 16,
+		16, path.Join(chain.config.User.DataDir(), chain.config.Dev.DB2FreezerName),
+		chain.config.Dev.DB2Name, false)
 
-// 	err = state.DB().DB().Update(func(tx *bbolt.Tx) error {
-// 		bucketName := metadata.GetBlockBucketName(parentHeaderHash)
-// 		blockBucket := tx.Bucket([]byte(bucketName))
-// 		if blockBucket == nil {
-// 			_, err := tx.CreateBucket(bucketName)
-// 			if err != nil {
-// 				return fmt.Errorf("create bucket: %s", err)
-// 			}
-// 			return nil
-// 		}
+	if err != nil {
+		log.Error("Failed to create db2")
+		t.Error(err)
+	}
 
-// 		return nil
-// 	})
+	chain.db2 = state2.NewDatabaseWithConfig(db2, nil)
 
-// 	if err != nil {
-// 		t.Errorf("unexpected error committing to database (%v)", err)
-// 	}
+	networkId := uint64(1)
+	timestamp := ntp.GetNTP().Time()
+	blockProposer := dilithium.New()
+	blockProposerPK := blockProposer.GetPK()
+	addrTo := xmss.NewXMSSFromHeight(10, 0).GetAddress()
+	masterXmss := xmss.NewXMSSFromHeight(4, 0)
+	masterXmssPK := masterXmss.GetPK()
+	//txnHash := sha256.Sum256([]byte("txHash"))
+	var txs []*protos.Transaction
+	amount := uint64(30)
+	fee := uint64(1)
+	message := []byte("message")
+	nonce := uint64(30)
+	networkID := uint64(1)
+	txn1 := transactions.NewTransfer(networkID, addrTo[:], amount, fee, 0, message, nonce, masterXmssPK[:])
+	txs = append(txs, txn1.PBData())
 
-// 	err = state.DB().DB().Update(func(tx *bbolt.Tx) error {
-// 		bucketName := metadata.GetBlockBucketName(parentHeaderHash2)
-// 		blockBucket := tx.Bucket([]byte(bucketName))
-// 		if blockBucket == nil {
-// 			_, err := tx.CreateBucket(bucketName)
-// 			if err != nil {
-// 				return fmt.Errorf("create bucket: %s", err)
-// 			}
-// 			return nil
-// 		}
+	protocolTxs := make([]*protos.ProtocolTransaction, 0)
+	lastCoinBaseNonce := uint64(10)
 
-// 		return nil
-// 	})
+	newBlock := block.NewBlock(networkId, timestamp, blockProposerPK[:], 0, parentHeaderHash2, txs, protocolTxs, lastCoinBaseNonce)
+	newBlock.PBData().Header.Hash = block.ComputeBlockHash(newBlock).Bytes()
+	bytesBlock, _ := newBlock.Serialize()
+	//chain.AddBlock(newBlock)
+	err = state.DB().DB().Update(func(tx *bbolt.Tx) error {
+		mainBucket := tx.Bucket([]byte("DB"))
+		if mainBucket == nil {
+			_, err := tx.CreateBucket([]byte("DB"))
+			if err != nil {
+				return fmt.Errorf("create bucket: %s", err)
+			}
+			return nil
+		}
 
-// 	if err != nil {
-// 		t.Errorf("unexpected error committing to database (%v)", err)
-// 	}
+		err := mainChainMetaData.Commit(mainBucket)
+		if err != nil {
+			return err
+		}
+		err = parentBlockMetadata.Commit(mainBucket)
+		if err != nil {
+			return err
+		}
+		err = parentBlockMetadata2.Commit(mainBucket)
+		if err != nil {
+			return err
+		}
+		err = epochMetadata.Commit(mainBucket)
+		if err != nil {
+			return err
+		}
 
-// 	db2, err := rawdb.NewLevelDBDatabaseWithFreezer(
-// 		path.Join(chain.config.User.DataDir(), chain.config.Dev.DB2Name), 16,
-// 		16, path.Join(chain.config.User.DataDir(), chain.config.Dev.DB2FreezerName),
-// 		chain.config.Dev.DB2Name, false)
+		err = mainBucket.Put(block.GetBlockStorageKey(parentHeaderHash), bytesBlock)
+		if err != nil {
+			log.Error("[Commit] Failed to commit block")
+			return err
+		}
 
-// 	if err != nil {
-// 		log.Error("Failed to create db2")
-// 		t.Error(err)
-// 	}
+		return nil
+	})
 
-// 	chain.db2 = state2.NewDatabaseWithConfig(db2, nil)
+	if err != nil {
+		t.Errorf("unexpected error committing to database (%v)", err)
+	}
+	_, err = block.GetBlock(state.DB(), parentHeaderHash)
+	if err != nil {
+		t.Error(err)
+	}
 
-// 	networkId := uint64(1)
-// 	timestamp := ntp.GetNTP().Time()
-// 	blockProposer := dilithium.New()
-// 	blockProposerPK := blockProposer.GetPK()
-// 	addrTo := xmss.NewXMSSFromHeight(10, 0).GetAddress()
-// 	masterXmss := xmss.NewXMSSFromHeight(4, 0)
-// 	masterXmssPK := masterXmss.GetPK()
-// 	//txnHash := sha256.Sum256([]byte("txHash"))
-// 	var txs []*protos.Transaction
-// 	amount := uint64(30)
-// 	fee := uint64(1)
-// 	message := []byte("message")
-// 	nonce := uint64(30)
-// 	networkID := uint64(1)
-// 	txn1 := transactions.NewTransfer(networkID, addrTo[:], amount, fee, 0, message, nonce, masterXmssPK[:])
-// 	txs = append(txs, txn1.PBData())
+	// newBlock2 := block.NewBlock(networkId, timestamp, blockProposerPK[:], parentSlotNumber, parentHeaderHash2, txs, protocolTxs, lastCoinBaseNonce)
+	// newBlock2.PBData().Header.Hash = block.ComputeBlockHash(newBlock2).Bytes()
+	// chain.AddBlock(newBlock2)
 
-// 	protocolTxs := make([]*protos.ProtocolTransaction, 0)
-// 	lastCoinBaseNonce := uint64(10)
-
-// 	newBlock := block.NewBlock(networkId, timestamp, blockProposerPK[:], slotNumber, parentHeaderHash2, txs, protocolTxs, lastCoinBaseNonce)
-// 	newBlock.PBData().Header.Hash = block.ComputeBlockHash(newBlock).Bytes()
-// 	chain.AddBlock(newBlock)
-// 	_, err = block.GetBlock(state.DB(), parentHeaderHash)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-
-// 	newBlock2 := block.NewBlock(networkId, timestamp, blockProposerPK[:], parentSlotNumber, parentHeaderHash2, txs, protocolTxs, lastCoinBaseNonce)
-// 	newBlock2.PBData().Header.Hash = block.ComputeBlockHash(newBlock2).Bytes()
-// 	chain.AddBlock(newBlock2)
-
-// 	_, err = chain.GetSlotLeaderDilithiumPKBySlotNumber(trieRoot, slotNumber, parentHeaderHash)
-// 	if err != nil {
-// 		t.Error("got unexpected error while fetching slot leader: ", err)
-// 	}
-// }
+	_, err = chain.GetSlotLeaderDilithiumPKBySlotNumber(trieRoot, slotNumber, parentHeaderHash)
+	if err != nil {
+		t.Error("got unexpected error while fetching slot leader: ", err)
+	}
+}
